fe64e012365c6a9d0289a65794ef120735e93058
Kevin Haggerty <khaggerty@signiant.com>
2017-12-18 13:25:37 -0500

    Fix get_active_task_defs and get_tasks_with_name_in_cluster functions to handle nextToken properly

--- task_cleanup.py
+++ task_cleanup.py
@@ -37,6 +37,7 @@ def cleanup_tasks(task_prefix, max_age, cluster_name=None, exclude_filters=[], n
             if 'HTTPStatusCode' in query_result['ResponseMetadata']:
                 if query_result['ResponseMetadata']['HTTPStatusCode'] == 200:
                     if 'nextToken' in query_result:
+                        result.extend(query_result['families'])
                         result.extend(get_active_task_defs(task_prefix=task_prefix,
                                                            next_token=query_result['nextToken']))
                     else:
@@ -55,6 +56,7 @@ def cleanup_tasks(task_prefix, max_age, cluster_name=None, exclude_filters=[], n
             if 'HTTPStatusCode' in query_result['ResponseMetadata']:
                 if query_result['ResponseMetadata']['HTTPStatusCode'] == 200:
                     if 'nextToken' in query_result:
+                        result.extend(query_result['taskArns'])
                         result.extend(get_tasks_with_name_in_cluster(task_name=task_name,
                                                                      cluster_name=cluster_name,
                                                                      next_token=query_result['nextToken']))
